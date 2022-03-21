29d5c542226b78fbb9a3ac3d1dfd3d00767b82ab
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-03-21 14:24:12 +0400

    delete function added

--- js/script.js
+++ js/script.js
@@ -60,44 +60,80 @@ class ToDoList{
             this.inputText.value = "";
             this.msgEl.innerText="";
             this.inputText.style.cssText="border-bottom: 1p solid black";
-            this.listEl.innerHTML = this.listToHtml(toDoList);
+            this.listToHtml(toDoList);
         }
         else this.notify(validToDo.msg, this.inputText);
+    }
+    createTodo(toDo){
+        const li = document.createElement("li");
+        const navDiv = document.createElement("div");
+        const spanEdit = document.createElement("span");
+        const spanDelete = document.createElement("span");
+        const input = document.createElement("input");
+        const doneDiv = document.createElement("div");
+
+        navDiv.setAttribute("class", "edit-delete");
+
+        spanEdit.setAttribute("class", "edit-icon");
+        spanEdit.innerHTML = `<i class="fa fa-edit"></i>`;
+
+        spanDelete.setAttribute("class", "delete-icon");
+        spanDelete.innerHTML = `<i class="fa fa-trash-o"></i>`;
+
+
+        navDiv.append(spanEdit);
+        navDiv.append(spanDelete);
+
+        li.append(navDiv);
+
+        input.setAttribute("class", "to-do-text");
+        input.setAttribute("value", toDo.name);
+        input.setAttribute("disabled", true);
+
+       
+        li.append(input);
+
+        doneDiv.setAttribute("class", "done");
+        doneDiv.innerHTML = ` <i class="fa fa-check-circle"></i>`;
+
+        li.append(doneDiv);
+
+        spanEdit.addEventListener("click", ()=>this.delete(toDo.id));
+        spanDelete.addEventListener("click", ()=>this.delete(toDo.id));
+        
+        this.listEl.append(li);
     }
     edit(){
 
     }
-    delete(){
-
+    delete(id){
+        let toDoList = this.getData();
+        const newToDos = toDoList.filter(item=>item.id!==id?true:false);
+        localStorage.setItem("toDoList", JSON.stringify(newToDos));
+        toDoList = this.getData();
+        if(toDoList.length < 1 ) {
+            localStorage.removeItem("toDoList");
+            this.notify("To-do list is empty");
+        }
+        this.listToHtml(toDoList);
     }
     listToHtml(todos){
-        let list = "";
-        todos.forEach(toDo=>{
-            list+=`
-                <li>
-                    <div class="edit-delete">
-                        <span class="edit-icon"><i class="fa fa-edit"></i></span>
-                        <span class="delete-icon"><i class="fa fa-trash-o"></i></span>
-                    </div>
-                <input class="to-do-text" value="${toDo.name}" disabled>
-                    <div class="done">
-                        <i class="fa fa-check-circle"></i>
-                    </div>
-                </li>`;
-        });
-        return list;
+        this.listEl.innerHTML = "";
+        todos.forEach(toDo=>this.createTodo(toDo));
     }
     use(){
-        const toDoList = this.getData();
-        if(toDoList) this.listEl.innerHTML = this.listToHtml(toDoList);
+        const toDoList = this.getData();//retrive initial data
+
+        if(toDoList) this.listToHtml(toDoList);//render initial data
         else this.notify("To-do list is empty");
-        this.addButton.onclick = ()=>this.add();
+
+        this.addButton.onclick = () =>this.add();//add todo
     }
 }
 const inputText = document.querySelector("#to-do-input");
 const addButton = document.querySelector("#to-do-add");
 const deleteButton = document.querySelectorAll(".delete-icon");
-const editButton = document.querySelector(".edit-icon");
+const editButton = document.querySelectorAll(".edit-icon");
 const msgEl = document.querySelector("#msg");
 const listEl = document.querySelector("#to-do-list > ul");
 
