eb5ee8c2e6c45292f345eb76ff5e14d2baf2d4b7
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-03-23 14:12:40 +0400

    spaces problem solved

--- js/script.js
+++ js/script.js
@@ -1,145 +1,144 @@
 class ToDoList{
-    constructor(addButton, deleteButton, editButton, msgEl, inputText, listEl){
-        this.addButton = addButton;
-        this.deleteButton = deleteButton;
-        this.editButton = editButton;
-        this.msgEl = msgEl;
-        this.inputText = inputText;
-        this.listEl = listEl;
-    }
+  constructor(addButton, deleteButton, editButton, msgEl, inputText, listEl){
+    this.addButton = addButton;
+    this.deleteButton = deleteButton;
+    this.editButton = editButton;
+    this.msgEl = msgEl;
+    this.inputText = inputText;
+    this.listEl = listEl;
+  }
 
-    getData(){
-        if(localStorage.toDoList) return JSON.parse(localStorage.toDoList);
-        return false;
-    }
+  getData() {
+    if(localStorage.toDoList) return JSON.parse(localStorage.toDoList);
 
-    validate(name){
-        let answer = true;
-        if(name.length >= 5){
-            const toDoList = this.getData();
-            if(toDoList){
-                for (let i = 0; i < toDoList.length; i++) {
-                    if(toDoList[i].name === name){
-                        answer = {};
-                        answer.valid = false;
-                        answer.msg = `To-do: ${name} already exists`;
-                        break;
-                    }
-                }
-            }
-        }
-        else {
+    return false;
+  }
+
+  validate(name) {
+    let answer = true;
+    if(name.length >= 5){
+      const toDoList = this.getData();
+      if(toDoList) {
+
+        for (let i = 0; i < toDoList.length; i++) {
+
+          if(toDoList[i].name === name) {
             answer = {};
             answer.valid = false;
-            answer.msg = "To-do must have at least 5 symbols length";
+            answer.msg = `To-do: ${name} already exists`;
+            break;
+          }
         }
-        return answer;
+      }
+    } else {
+      answer = {};
+      answer.valid = false;
+      answer.msg = "To-do must have at least 5 symbols length";
+    }
+    return answer;
+  }
+
+  notify(msg, elementRef) {
+    elementRef ? elementRef.style.cssText = "border-bottom: 1px solid red" : false;
+    this.msgEl.innerText = msg;
+  }
+
+  add() {
+    let toDoList = this.getData();
+    const toDo = {};
+
+    if(toDoList) {
+      const lastId = toDoList[toDoList.length - 1].id;
+      toDo.id = lastId + 1;
+      toDo.name = this.inputText.value;
+      toDo.done = false;
+    } else {
+      toDoList = [];
+      toDo.id = 1;
+      toDo.name = this.inputText.value;
+      toDo.done = false;
     }
 
-    notify(msg, elementRef){
-        elementRef ? elementRef.style.cssText = "border-bottom: 1px solid red" : false;
-        this.msgEl.innerText = msg;
+    const validToDo = this.validate(toDo.name);
+
+    if(validToDo === true){
+      toDoList.push(toDo);
+      localStorage.setItem("toDoList", JSON.stringify(toDoList));
+      this.inputText.value = "";
+      this.msgEl.innerText = "";
+      this.inputText.style.cssText = "border-bottom: 1px solid black";
+      this.listToHtml(toDoList);
     }
 
-    add(){
-        let toDoList = this.getData();
-        const toDo = {};
+    else this.notify(validToDo.msg, this.inputText);
+  }
 
-        if(toDoList){
-            const lastId = toDoList[toDoList.length - 1].id;
-            toDo.id = lastId + 1;
-            toDo.name = this.inputText.value;
-            toDo.done = false;
-        }
+  setDone(id) {
+    const updateTodos = this.getData();
+    const foundInd = updateTodos.findIndex(toDo => toDo.id == id);
 
-        else{
-            toDoList = [];
-            toDo.id = 1;
-            toDo.name = this.inputText.value;
-            toDo.done = false;
-        }
-        const validToDo = this.validate(toDo.name);
-
-        if(validToDo === true){
-            toDoList.push(toDo);
-            localStorage.setItem("toDoList", JSON.stringify(toDoList));
-            this.inputText.value = "";
-            this.msgEl.innerText = "";
-            this.inputText.style.cssText = "border-bottom: 1px solid black";
-            this.listToHtml(toDoList);
-        }
-
-        else this.notify(validToDo.msg, this.inputText);
+    if(foundInd > -1) {
+      updateTodos[foundInd].done = !updateTodos[foundInd].done;
     }
 
-    setDone(id){
-        const updateTodos = this.getData();
-        const foundInd = updateTodos.findIndex(toDo => toDo.id == id);
+    localStorage.setItem("toDoList", JSON.stringify(updateTodos));
+    this.listToHtml(updateTodos);
+  }
 
-        if(foundInd > -1) updateTodos[foundInd].done = !updateTodos[foundInd].done; 
+  createTodo(toDo) {
+    const li = document.createElement("li");
+    const navDiv = document.createElement("div");
+    const spanEdit = document.createElement("span");
+    const spanDelete = document.createElement("span");
+    const spanCheck = document.createElement("span");
+    const input = document.createElement("input");
+    const doneDiv = document.createElement("div");
+    navDiv.setAttribute("class", "edit-delete");
+    spanEdit.setAttribute("class", "edit-icon");
+    spanEdit.innerHTML = `<i class="fa fa-edit"></i>`;
+    spanDelete.setAttribute("class", "delete-icon");
+    spanDelete.innerHTML = `<i class="fa fa-trash-o"></i>`;
+    spanCheck.setAttribute("class", "check-done");
+    spanCheck.innerHTML = `<i class="fa fa-check-circle"></i>`;
+    navDiv.append(spanEdit);
+    navDiv.append(spanDelete);
+    li.append(navDiv);
+    input.setAttribute("value", toDo.name);
 
-        localStorage.setItem("toDoList", JSON.stringify(updateTodos));
-        this.listToHtml(updateTodos);
+    if(toDo.done) {
+      input.classList.add("to-do-done", "to-do-text")
+    } else {
+      input.setAttribute("class", "to-do-text");
     }
 
-    createTodo(toDo){
-        const li = document.createElement("li");
-        const navDiv = document.createElement("div");
-        const spanEdit = document.createElement("span");
-        const spanDelete = document.createElement("span");
-        const spanCheck = document.createElement("span");
-        const input = document.createElement("input");
-        const doneDiv = document.createElement("div");
+    input.setAttribute("disabled", true);
+    li.append(input);
+    doneDiv.setAttribute("class", "done");
+    doneDiv.append(spanCheck);
+    li.append(doneDiv);
 
-        navDiv.setAttribute("class", "edit-delete");
-
-        spanEdit.setAttribute("class", "edit-icon");
-        spanEdit.innerHTML = `<i class="fa fa-edit"></i>`;
-
-        spanDelete.setAttribute("class", "delete-icon");
-        spanDelete.innerHTML = `<i class="fa fa-trash-o"></i>`;
+    spanCheck.addEventListener("click", () => this.setDone(toDo.id));
 
 
-        spanCheck.setAttribute("class", "check-done");
-        spanCheck.innerHTML = `<i class="fa fa-check-circle"></i>`;
+    this.listEl.append(li);
+  }
 
+  listToHtml(todos) {
+    this.listEl.innerHTML = "";
 
-        navDiv.append(spanEdit);
-        navDiv.append(spanDelete);
+    todos.sort( (a, b) => a.done > b.done ? 1 : -1 );
 
-        li.append(navDiv);
+    todos.forEach( toDo => this.createTodo(toDo) );
+  }
 
-        input.setAttribute("value", toDo.name);
-        toDo.done ? input.classList.add("to-do-done", "to-do-text") : input.setAttribute("class", "to-do-text");
-        input.setAttribute("disabled", true);
+  use() {
+    const toDoList = this.getData();//retrive initial data
 
-       
-        li.append(input);
+    if(toDoList) this.listToHtml(toDoList);//render initial data
+    else this.notify("To-do list is empty");
 
-        doneDiv.setAttribute("class", "done");
-        doneDiv.append(spanCheck);
-
-        li.append(doneDiv);
-        spanCheck.addEventListener("click", () => this.setDone(toDo.id));
-
-
-        this.listEl.append(li);
-    }
-
-    listToHtml(todos){
-        this.listEl.innerHTML = "";
-        todos.sort((a, b) => a.done > b.done ? 1 : -1);
-        todos.forEach(toDo => this.createTodo(toDo));
-    }
-
-    use(){
-        const toDoList = this.getData();//retrive initial data
-
-        if(toDoList) this.listToHtml(toDoList);//render initial data
-        else this.notify("To-do list is empty");
-
-        this.addButton.onclick = () => this.add();//add todo
-    }
+    this.addButton.onclick = () => this.add();//add todo
+  }
 }
 const inputText = document.querySelector("#to-do-input");
 const addButton = document.querySelector("#to-do-add");
